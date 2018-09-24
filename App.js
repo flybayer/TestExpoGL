6ebdf1bfd34175a3e94ffd948f21bf2761ffb860
Brandon Bayer <b@bayer.ws>
2018-09-24 12:47:24 -0400

    new: add empty black GLView

--- App.js
+++ App.js
@@ -6,44 +6,51 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react"
+import { Platform, StyleSheet, Text, View } from "react-native"
+import { GLView } from "expo-gl"
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android: "Double tap R on your keyboard to reload,\n" + "Shake or press menu button for dev menu",
+})
 
-type Props = {};
+type Props = {}
 export default class App extends Component<Props> {
+  onContextCreate = gl => {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    gl.endFrameEXP()
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+
+        <GLView style={{ flex: 1 }} onContextCreate={this.onContextCreate} />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: "center",
+    color: "#333333",
     marginBottom: 5,
   },
-});
+})
